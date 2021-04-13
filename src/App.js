244e1d0a63f9bf5cfda76c81eefc8875aec848c0
stuartbentley <sbentley@emerald.com>
2021-04-13 11:26:16 +0100

    moved createTodo into component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import { createTodo, updateTodo } from './graphql/mutations';
+import { updateTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
-import Button from '@material-ui/core/Button';
+import CreateTodo from './components/todo/create';
+
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import awsExports from "./aws-exports";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  }
+}));
 
 Amplify.configure(awsExports);
 
-const initialState = { name: '', url: '' }
-
 const App = () => {
-  const [formState, setFormState] = useState(initialState)
+
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
     fetchTodos()
   }, [])
-
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
-  }
 
   async function fetchTodos() {
     try {
@@ -29,31 +40,21 @@ const App = () => {
       const todos = todoData.data.listTodos.items
       console.log(todos);
       setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
-  }
-
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.url) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
     } catch (err) {
-      console.log('error creating todo:', err)
+      console.log('error fetching todos')
     }
   }
 
-  async function toggleComplete (event, todo) {
+  async function toggleComplete(event, todo) {
 
     const updatedTodo = {
-      id : todo.id,
+      id: todo.id,
       status: !todo.status
     }
 
     try {
       await API.graphql(graphqlOperation(updateTodo, {input: updatedTodo}))
-          .then( (response)=> {
+          .then((response) => {
             updateChanges(response);
           })
     } catch (err) {
@@ -67,37 +68,43 @@ const App = () => {
     }
   }
 
+  const classes = useStyles();
+
   return (
       <div style={styles.container}>
         <h2>React Learning Todos</h2>
-        <input
-            onChange={event => setInput('name', event.target.value)}
-            style={styles.input}
-            value={formState.name}
-            placeholder="Title"
-        />
-        <input
-            onChange={event => setInput('url', event.target.value)}
-            style={styles.input}
-            value={formState.url}
-            placeholder="URL"
-        />
-        <Button variant="contained" color="primary" onClick={addTodo}>Create Todo</Button>
-        {
-          todos.map((todo, index) => (
-              <div key={todo.id ? todo.id : index} style={styles.todo}>
-                <a href={todo.url}><p style={styles.todoName}>{todo.url}</p></a>
-                <FormControlLabel
-                    control={<Switch checked={todo.status} onChange= {(event) => {
-                      toggleComplete(event, todo);
-                    }} />}
-                    label="Completed"
-                />
-              </div>
+
+        <CreateTodo addedTodo={ todo => {setTodos([...todos, todo])} }></CreateTodo>
+        <div className={classes.root}>
+          {todos.map((todo, index) => (
+              <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id={todo.id ? todo.id : index}
+                    display="flex" flexDirection="column"
+                >
+                  <Typography className={classes.heading}>{todo.name}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <a href={todo.url}><p>{todo.url}</p></a>
+                  <Typography>
+                    {todo.description}
+                  </Typography>
+                  <FormControlLabel
+                      control={<Switch checked={todo.status} onChange={(event) => {
+                        toggleComplete(event, todo);
+                      }}/>}
+                      label="Completed"
+                  />
+
+                </AccordionDetails>
+              </Accordion>
           ))
-        }
+          }
+        </div>
       </div>
-  )
+  );
 }
 
 const styles = {
